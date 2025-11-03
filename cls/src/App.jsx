816e758e7a34809fe3261a0fc2433ca10ef0c021
mufeedha-tm <mufeedha059@gmail.com>
2025-11-03 13:04:43 +0530

    first commit

---
+++ cls/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Welcome from './embedding-expression/Welcome'
+// import Basic from './Basic'
+
+function App() {
+
+  return (
+    <>
+    {/* ----embedding-expression---- */}
+    {/* <Basic/> */}
+    <Welcome/>
+    </>
+  )
+}
+
+export default App
